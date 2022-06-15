0ea5a0a47bbc6a0fbd39662c421267cf0e29f856
Rumshel <96851834+Rumshel@users.noreply.github.com>
2022-06-15 11:32:12 -0400

    added bootstrap and container

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Julie Ruminski-Shelton and is open-sourced on
-        <a
-          href="https://github.com/Rumshel/new-react-weather.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by Julie Ruminski-Shelton and is open-sourced
+          on
+          <a
+            href="https://github.com/Rumshel/new-react-weather.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
