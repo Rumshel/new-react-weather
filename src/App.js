0402a8c49d57d7f31d57a21dbc13486e099bdcf9
Rumshel <96851834+Rumshel@users.noreply.github.com>
2022-06-15 11:11:38 -0400

    updated app links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Julie Ruminski-Shelton and is open-sourced on
+        <a
+          href="https://github.com/Rumshel/new-react-weather.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
