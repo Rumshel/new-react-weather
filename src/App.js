92c72cd6ac4d910234a6a77f6887fda2e9b8d416
Rumshel <96851834+Rumshel@users.noreply.github.com>
2022-06-16 10:16:49 -0400

    added Weather pages; added HTML

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        Weather App
+        <Weather />
         <footer>
           This project was coded by Julie Ruminski-Shelton and is open-sourced
           on
